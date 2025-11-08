eb824029ad75bfd3de9ff9374104058eea93a4de
NIVETHA123125 <nivethavivekananthan1806@gmail.com>
2025-11-08 23:32:52 +0530

    hey

--- patterns.java
+++ patterns.java
@@ -1,15 +1,33 @@
 import java.util.*;
 class patterns{
-    public static void main(String [] args){
+    public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        int n=6;
-        for(int i=1;i<=n;i++){
-            for(int j=n-i;j>1;j--){
-                for(j=1;j<n;j++){
-                   System.out.print("* ");
-                }
-            }
-            System.out.println();
+        System.out.println("enter the symbol");
+        String n=sc.nextLine();
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        switch(n){
+            case "+":
+                System.out.println("add: "+(i+j));
+                break;
+            case "-":
+                System.out.println("sub: "+(i-j));
+                break;
+            case "*":
+                System.out.println("multiply: "+(i*j));
+                break;
+            case "/":
+                System.out.println("divide: "+(i/j));
+                break;
+            case "%":
+                System.out.println("modulus: "+(i%j));
+                break;
+            default:
+                System.out.println("error");
+                break;
+
+            
+
         }
     }
 }
