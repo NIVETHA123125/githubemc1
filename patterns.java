3faafffd38b84544eb0b78ed1fd18a9c6cb64ff2
NIVETHA123125 <nivethavivekananthan1806@gmail.com>
2025-11-08 23:41:45 +0530

    byee

--- patterns.java
+++ patterns.java
@@ -2,32 +2,25 @@ import java.util.*;
 class patterns{
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the symbol");
-        String n=sc.nextLine();
-        int i=sc.nextInt();
-        int j=sc.nextInt();
-        switch(n){
-            case "+":
-                System.out.println("add: "+(i+j));
-                break;
-            case "-":
-                System.out.println("sub: "+(i-j));
-                break;
-            case "*":
-                System.out.println("multiply: "+(i*j));
-                break;
-            case "/":
-                System.out.println("divide: "+(i/j));
-                break;
-            case "%":
-                System.out.println("modulus: "+(i%j));
-                break;
-            default:
-                System.out.println("error");
-                break;
-
-            
+        System.out.println("enter weight");
+        int weight=sc.nextInt();
+        System.out.println("enter height");
+        int height=sc.nextInt();
+        int bmi=weight/(height*height);
+        if(bmi<18.5){
+            System.out.println("thinnu nalla");
+        }
+        else if(bmi>=18.5 && bmi<25){
+            System.out.println("maintain pannu");
+        
+        }
+        else if(bmi>=25 && bmi<30){
+            System.out.println("gundu agita da eruma");
 
         }
+        else{
+            System.out.println("weight kora da panni payale");
+        }
+        
     }
 }
